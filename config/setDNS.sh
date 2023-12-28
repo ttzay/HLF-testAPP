4a158cc7892bfddd44939d809bc171c20a439e8e
ttzay <zhou.yuz@whu.edu.cn>
2023-12-28 10:43:17 +0000

    Add setDNS

---
+++ config/setDNS.sh
@@ -0,0 +1,12 @@
+echo "172.28.7.34       council.ifantasy.net" >> /etc/hosts
+echo "172.28.7.34       soft.ifantasy.net" >> /etc/hosts
+echo "172.28.7.35       web.ifantasy.net" >> /etc/hosts
+echo "172.28.7.35       hard.ifantasy.net" >> /etc/hosts
+
+echo "172.28.7.34       orderer1.council.ifantasy.net" >> /etc/hosts
+echo "172.28.7.34       orderer2.council.ifantasy.net" >> /etc/hosts
+echo "172.28.7.35       orderer3.council.ifantasy.net" >> /etc/hosts
+
+echo "172.28.7.34       peer1.soft.ifantasy.net" >> /etc/hosts
+echo "172.28.7.35       peer1.web.ifantasy.net" >> /etc/hosts
+echo "172.28.7.35       peer1.hard.ifantasy.net" >> /etc/hosts
